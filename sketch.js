1c4db79efe2ac5d6e49e242466378b2ad5fdfade
tanay6june <72078342+tanay6june@users.noreply.github.com>
2021-02-09 19:02:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,25 +3,26 @@ var database;
 var height;
 
 function preload(){
-  balloon_image = loadAnimation("Hot Air Balloon-02.png,Hot Air Balloon-03.png,Hot Air Balloon-04.png")
+  balloon_image = loadAnimation("HAB2.png","HAB3.png","HAB4.png")
+  b1=loadImage("HAB1.png")
 }
 
 function setup() {
   database = firebase.database();
-  console.log(database);
+  //console.log(database);
 
   createCanvas(500,500);
   balloon = createSprite(250,250,10,10);
 
   var balloonPosition = database.ref('balloon/position');
-  balloonPosition.on("value",readPosition,showError); 
+  balloonPosition.on("value",readHeight,showError); 
 }
 
 function draw() {
-  background("Hot Air Balloon-01.png"); 
+  background(b1); 
 
-  Stroke();
-  textSize(35)
+  //stroke();
+  textSize(35);
   fill("white")
   text("USE ARROW KEYS TO MOVE THE HOT AIR BALLOON",10, 50)
 
@@ -29,17 +30,20 @@ function draw() {
   {
   
   if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloon_image);
     balloon.x = balloon.x-10;
-}
-else if(keyDown(RIGHT_ARROW)){
+   }
+   else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloon_image);
     balloon.x = balloon.x+10;
-}
-else if(keyDown(UP_ARROW)){
+  }
+  else if(keyDown(UP_ARROW)){
   updateHeight(0,-10);
   balloon.addAnimation("hotAirBalloon",balloon_image);
-  balloon.scale=balloon.scale-0.4;
-}
-else if(keyDown(DOWN_ARROW)){
+  balloon.scale=balloon.scale-0.3;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloon_image);
   balloon.y = balloon.y+10;
 }
 
@@ -50,13 +54,13 @@ drawSprites();
 function updateHeight(x,y){
   database.ref('balloon/position').set({
      'x' : height.x + x,
-     'y' : height.y + y,
+     'y' : height.y + y
   })
   }
 
 function readHeight(data){
    height = data.val();
-   console.log(position.x);
+   //console.log(position.x);
    balloon.x = height.x;
    balloon.y = height.y;
 }   
